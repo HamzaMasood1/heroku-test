f8c61dcb77763df761ad1e181a510afcb07dd0a0
HamzaMasood1 <hamzamasood183@gmail.com>
2023-11-13 00:49:56 +0000

    testing third party library

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/HamzaMasood1/heroku-test
 
 go 1.21.3
 
-require github.com/russross/blackfriday v1.6.0
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/russross/blackfriday v1.6.0
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"github.com/russross/blackfriday"
 	"net/http"
 	"os"
@@ -12,21 +13,48 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+	router := httprouter.New()
+	router.GET("/", Index)
 
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.HandleFunc("/test", TestingHandler)
+	router.GET("/hello/:name", Hello)
 
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	//http.HandleFunc("/markdown", GenerateMarkdown)
+	//http.HandleFunc("/test", TestingHandler)
+	//
+	//http.Handle("/", http.FileServer(http.Dir("public")))
 
-	http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		return
+	}
+}
+
+func Hello(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	_, err := fmt.Fprintf(writer, "Hello, %s!\n", params.ByName("name"))
+	if err != nil {
+		return
+	}
+}
+
+func Index(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	_, err := fmt.Fprintln(writer, "Welcome!")
+	if err != nil {
+		return
+	}
 }
 
 func TestingHandler(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("Hello"))
+	_, err := writer.Write([]byte("Hello"))
+	if err != nil {
+		return
+	}
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
 	fmt.Println(r.FormValue("body"))
-	rw.Write(markdown)
+	_, err := rw.Write(markdown)
+	if err != nil {
+		return
+	}
 }
